baba582fb09d162a8d10de93d2a7d1120f62b948
Grant Williamson <Grant.Williamson@utas.edu.au>
2019-09-03 12:15:01 +1000

    Fixed logits

--- run2.r
+++ run2.r
@@ -28,65 +28,6 @@ mask_tif<-raster(paste0(rast_temp,"/roi_mask.tif"))
 
 
 
-rx_write=function(file,outfile){
-  require(foreign)
-  require(sp)
-  require(raster)
-  rtable = data.frame(ID = c(1,2,3,4,5,6,7,8,9),
-                      Status = c("NoFireRegime",
-                                 "TooFrequentlyBurnt",
-                                 "Vulnerable",
-                                 "LongUnburnt",
-                                 "WithinThreshold",
-                                 "Recently Treated",
-                                 "Monitor OFH In the Field",
-                                 "Priority for Assessment and Treatment",
-                                 "Unknown"))
-  col_vec = c("#ffffff",
-              "#ffffff","#ff0000","#ff6600","#00ffff","#999999","#99FF99","#226622","#00ff00","#cccccc")
-  
-  tr <- raster(paste0(rast_temp,"/",file))
-  tr = tr * mask_tif
-  tr <- ratify(tr)
-  rat <- levels(tr)[[1]]
-  rat <- left_join(rat,rtable)
-  levels(tr) <- rat
-  colortable(tr) <- col_vec
-  
-  # Write ESRI DB
-  
-  atable = levels(tr)[[1]]
-  names(atable)=c("VALUE","CATEGORY")
-  x = as.data.frame(table(raster::values(tr)))
-  names(x)=c("VALUE","COUNT")
-  x$VALUE = as.numeric(as.character(x$VALUE))
-  a2 = left_join(atable,x)
-  a2$COUNT[is.na(a2$COUNT)]=0
-  a2 = dplyr::select(a2,VALUE,COUNT,CATEGORY)
-  write.dbf(a2,paste0(rast_temp,"/",outfile,".vat.dbf"))
-  
-  # Fix projection
-  
-  crs(tr) <- CRS('+init=epsg:3308')
-  
-  bigWrite(tr,paste0(rast_temp,"/",outfile))
-  unlink(paste0(rast_temp,"/",file))
-  
-}
-
-# Insert 3308 projection into TIF output
-esri_output = function(tfile){
-  log_it("Generating ESRI projection")
-  infile = paste0(rast_temp,"/",tfile)
-  tempfile = paste0(rast_temp,"/",tfile,".tmp")
-  gt = Sys.which("gdal_translate")
-  cmd=paste0(gt," ",infile," -a_srs 3308.prj -co COMPRESS=LZW ",tempfile)
-  cout = system(cmd,intern=TRUE)
-  log_it(cout)
-  unlink(infile)
-  file.rename(tempfile,infile)
-}
-
 
 #######
 
@@ -94,7 +35,7 @@ vegbase = read_sf(paste0(rast_temp,"/v_vegBase.gpkg"))
 vegcode = raster(paste0(rast_temp,"/r_vegcode.tif"))
 vegcode = vegcode * mask_tif
 
-log_it("Generating code table")
+print("Generating code table")
 codelist = tibble(ID=unique(vegbase[[f_vegid]]))
 codelist$category = ""
 vt = which(substr(names(vegbase),1,7)=="VEGTEXT")[1]
@@ -114,10 +55,10 @@ rat <- levels(tr)[[1]]
 rat <- left_join(rat,codelist)
 rat$category[is.na(rat$category)]=""
 
-log_it("Generating colour table")
+print("Generating colour table")
 col_vec = colorRampPalette(c("white","brown","green","red","blue","yellow","pink","purple"))(nrow(rat))
 
-log_it("Assigning colours and names")
+print("Assigning colours and names")
 ids = as.numeric(rownames(rat))
 codes = rat$ID
 
@@ -143,11 +84,11 @@ col_vec[1]="#ffffff"
 col_vec=c("#ffffff",col_vec)
 colortable(tr) <- col_vec
 
-log_it("Writing file")
+print("Writing file")
 bigWrite(tr,paste0(rast_temp,"/r_vegcode2.tif"))
 esri_output("r_vegcode2.tif")
 
-log_it("Removing old files")
+print("Removing old files")
 unlink(paste0(rast_temp,"/r_vegcode.tif"))
 unlink(paste0(rast_temp,"/r_vegcode.tif.aux.xml"))
 file.rename(paste0(rast_temp,"/r_vegcode2.tif"),paste0(rast_temp,"/r_vegcode.tif"))
